e4ef0e839a7320b83922a9e73ef64fc3711b89fd
Alejandro <alejo1282@outlook.com>
2022-04-05 12:28:50 -0400

    Created all of the functions needed for Queue-List

--- Lab Projects/A - Fundamentals of AI/Project/QueueList/QueueList_TODO.h
+++ Lab Projects/A - Fundamentals of AI/Project/QueueList/QueueList_TODO.h
@@ -14,21 +14,81 @@ namespace fullsail_ai { namespace fundamentals {
 	//!   - Define the <code>QueueList</code> destructor.
 	//!   - Define the <code>QueueList</code> methods.
 
+	template<typename T>
+	QueueList<T>::QueueList()
+	{
+	}
+
+	template<typename T>
+	QueueList<T>::~QueueList()
+	{
+	}
 
 	template <typename T>
 	typename QueueList<T>::Iterator& QueueList<T>::Iterator::operator++()
 	{
 		// TODO: Complete the definition of this operator.
-		return *this;
 	}
 
 	template <typename T>
 	T QueueList<T>::Iterator::operator*() const
 	{
 		// TODO: Complete the definition of this operator.
-
 	}
 
+	template <typename T>
+	bool QueueList<T>::isEmpty() const
+	{
+		// Check if the current node is null 
+		// return true is null
+		// else return false
+		
+	}
+
+	template <typename T>
+	void QueueList<T>::enqueue(T element)
+	{
+		// check if the list is not empty
+		// set the elements next to the front
+		// set the front to the element
+
+		// if it is empty set the front to element
+	}
+	
+	template <typename T>
+	T QueueList<T>::getFront() const
+	{
+		// return front
+	}
+
+	template <typename T>
+	void QueueList<T>::dequeue()
+	{
+		// Check if the fronts next is not null
+		// if not null create a temp of the front 
+		// set the front next to front 
+		// delete the temp
+		// else delete the front
+	}
+
+	template <typename T>
+	void QueueList<T>::removeAll()
+	{
+		// loop while the list is not empty
+		// de the same ads dequeue
+	}
+
+	template <typename T>
+	bool QueueList<T>::contains(T element) const
+	{
+		return false;
+	}
+
+	template <typename T>
+	void QueueList<T>::remove(T element)
+	{
+		
+	}
 
 }}  // namespace fullsail_ai::fundamentals
 
